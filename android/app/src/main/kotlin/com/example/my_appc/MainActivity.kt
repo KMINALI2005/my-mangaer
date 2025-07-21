1d584afe7b04b6be64af105007604f77e5073565
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-07-21 22:47:12 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/my_appc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_appc/MainActivity.kt
@@ -1,6 +1,18 @@
-package com.example.age_calculator
+package com.example.age_calculator_smart // تأكد من أن هذا السطر يطابق اسم الحزمة لديك
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import androidx.core.view.WindowCompat
 
 class MainActivity: FlutterActivity() {
+    // هذه الدالة تُستدعى بعد تهيئة محرك فلاتر
+    // نستخدمها لتخصيص واجهة الأندرويد الأصلية
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        // هذا السطر هو الأهم:
+        // يجعل واجهة فلاتر تمتد خلف أشرطة النظام (الحالة والتنقل)
+        // مما يسمح لنا بإنشاء تصميم يملأ الشاشة بالكامل
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
 }
